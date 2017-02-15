2966417ac5fda9d9da3a9ebd2307b8f00e824242
Chris Liu <chris.sj.liu@gmail.com>
2017-02-14 16:22:57 -0800

    save_fig -> savefig

--- src/count_data.py
+++ src/count_data.py
@@ -17,7 +17,7 @@ def plot_time_counts(count_dict,outname='../images/time_counts.png'):
     X,y = np.array(X),np.array(y)
 
     plt.plot(X,y)
-    plt.save_fig(outname)
+    plt.savefig(outname)
 
     return None
 
