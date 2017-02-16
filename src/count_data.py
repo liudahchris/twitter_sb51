afa70882763c50563ac26ff8766b780b2f83c29a
Chris Liu <chris.sj.liu@gmail.com>
2017-02-15 17:44:35 -0800

    update to count data and tweets

--- src/count_data.py
+++ src/count_data.py
@@ -2,6 +2,18 @@ from collections import defaultdict
 import pymongo
 from clean_date import _bin_date
 import numpy as np
+from string import punctuation
+from nltk.corpus import stopwords
+
+def clean_text(tweet,stopwords,punc):
+    '''
+    Takes a tweet, lowers and strips punctuation, and removes stopwords and tags
+    INPUT: string
+    OUTPUT: string
+    '''
+    tokens = tweet.lower().translate(None,punc).split()
+    tokens = [token for token in tokens if token[0]!='#' and token not in stopwords]
+    return ' '.join(tokens)
 
 def dict_to_csv(d,outname,key_names='col1',val_names='col2'):
     '''
@@ -15,6 +27,30 @@ def dict_to_csv(d,outname,key_names='col1',val_names='col2'):
             f.write('{}, {}\n'.format(key,d[key]))
     return None
 
+
+def aggregate_tweets(src_coll,target_coll,verbose=False):
+    '''
+    '''
+    START_TIME = "2017/02/05 22:30:00"
+    END_TIME = "2017/02/06 04:30:00"
+    STOPWORDS = stopwords.words('english')
+
+    # Punctuation to remove
+    # We'll keep @ to look at popular users and # to filter out hashtags
+    PUNCTUATION = string.punctuation.translate(None,'@#')
+
+    for i,tweet in enumerate(src_coll.find()):
+        print_status(i,verbose=VERBOSE)
+        # Bin time
+        time = _bin_date(tweet['created_at'],bin_size=60)
+        if time =< START_TIME or time >= END_TIME:
+            continue
+        text = clean_text(tweet['text'],stopwords=STOPWORDS,punc=PUNCTUATION)
+        # Add tweet text to time slot
+        target_coll.update_one({'time':time},{'$push':{'tweets':text}},upsert=True)
+    return None
+
+
 def print_status(i,n=10000,verbose=True):
     '''
     Prints statement after ever n tweets are processed
@@ -23,32 +59,28 @@ def print_status(i,n=10000,verbose=True):
         print "{} tweets processed".format(i)
     return
 
+
 def main():
     '''
-    This script will take clean data and do a raw count.
+    This script will take clean data and do a raw count and word count.
     The Super Bowl started about approx. Feb 05 2017 3:30pm pst
     and ended at about Feb 05 2017 7:30pm pst.
-
     These translate to Feb 05 22:30:00 UTC and Feb 06 4:30:00 UTC.
     The dates should be clean (from clean_tweets.py), so comparisons
     should make sense.
-    '''
-    START_TIME = "2017/02/05 22:30:00"
-    END_TIME = "2017/02/06 04:30:00"
 
+    We'll store this data in another collection in Mongo to
+    '''
     client = pymongo.MongoClient()
     db = client['clean_tweets']
     coll = db['clean_tweets']
+    target = db['binned_tweets']
 
-    tweet_counts = defaultdict(int)
     VERBOSE = False
 
-    for i,tweet in enumerate(coll.find()):
-        print_status(i,verbose=VERBOSE)
-        time = _bin_date(tweet['created_at'],bin_size=30)
-        if time < START_TIME or time > END_TIME:
-            continue
-        tweet_counts[time] += 1
+    aggregate_tweets(coll,target,VERBOSE)
+    
+
 
     client.close()
 
